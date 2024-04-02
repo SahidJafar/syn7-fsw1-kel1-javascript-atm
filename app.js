f904b5a7e27cefcd1518119299856d858592f583
nellamchya <88300976+nellamchya@users.noreply.github.com>
2024-04-02 13:39:39 +0700

    add feature validate pin

--- app.js
+++ app.js
@@ -1,93 +1,119 @@
 const readLine = require("readline");
 
 const rl = readLine.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
 });
 
 const accountA = {
-  name: "John Doe",
-  balance: 0,
-  pin: "123456",
-  cardNumber: "1234 5678 1234 5678",
-  transactions: [],
+    name: "John Doe",
+    balance: 0,
+    pin: "123456",
+    cardNumber: "1234 5678 1234 5678",
+    transactions: [],
 };
 
 const accountB = {
-  name: "Jane Doe",
-  balance: 0,
-  pin: "654321",
-  cardNumber: "5678 1234 5678 1234",
-  transactions: [],
+    name: "Jane Doe",
+    balance: 0,
+    pin: "654321",
+    cardNumber: "5678 1234 5678 1234",
+    transactions: [],
 };
 
 const accounts = [accountA, accountB];
 
 function validateCardNumber(cardNumber) {
-  return accounts.find((account) => account.cardNumber === cardNumber);
+    return accounts.find((account) => account.cardNumber === cardNumber);
 }
-function validatePin() {
-  // TODO: validate pin
+
+function validatePin(cardNumber, pin) {
+    // TODO: validate pin
+    const account = validateCardNumber(cardNumber);
+    if (account && account.pin === pin) {
+        return account;
+    } else {
+        return null;
+    }
 }
+
 function checkBalance() {
-  // TODO: check balance
+    // TODO: check balance
 }
+
 function deposit() {
-  // TODO: deposit
+    // TODO: deposit
 }
+
 function viewTransactions() {
-  // TODO: view transactions
+    // TODO: view transactions
 }
 
 function askQuestion(question) {
-  return new Promise((resolve) => {
-    rl.question(question, (answer) => {
-      resolve(answer);
+    return new Promise((resolve) => {
+        rl.question(question, (answer) => {
+            resolve(answer);
+        });
     });
-  });
 }
 
 async function main() {
-  let currentUser = null;
+    let currentUser = null;
 
-  while (!currentUser) {
-    const cardNumber = await askQuestion("Masukkan nomor kartu ATM anda: ");
-    const account = validateCardNumber(cardNumber);
+    while (!currentUser) {
+        const cardNumber = await askQuestion("Masukkan nomor kartu ATM anda: ");
+        const account = validateCardNumber(cardNumber);
 
-    if (account) {
-      const pin = await askQuestion("Masukkan PIN Anda: ");
-      // TODO: Validate pin
-    } else {
-      console.log("Nomor kartu tidak valid. Silahkan coba lagi.");
+        if (account) {
+            let pinAttempts = 0;
+            let pin = await askQuestion("Masukkan PIN Anda: ");
+            // TODO: Validate pin
+            let validateAccount = validatePin(cardNumber, pin);
+
+            while (!validateAccount && pinAttempts < 2) {
+                console.log("PIN salah. Silahkan coba lagi.");
+                pin = await askQuestion("Masukkan PIN Anda: ");
+                validateAccount = validatePin(cardNumber, pin);
+                pinAttempts++;
+            }
+            if (validateAccount) {
+                currentUser = validateAccount;
+                console.log(Selamat datang, $ { currentUser.name }!);
+            } else {
+                console.log("Anda telah memasukkan PIN yang salah sebanyak 3 kali. Silahkan coba lagi.");
+                rl.close();
+                return;
+            }
+        } else {
+            console.log("Nomor kartu tidak valid. Silahkan coba lagi.");
+        }
     }
-  }
+    do {
+        console.log("====================================");
+        console.log("Menu ATM:");
+        console.log("1. Cek Saldo");
+        console.log("2. Setor Tunai");
+        console.log("3. Riwayat Transaksi");
+        console.log("4. Keluar");
 
-  do {
+        choice = await askQuestion("Masukkan pilihan Anda: ");
+
+        switch (parseInt(choice)) {
+            case 1:
+                // TODO: check balance
+                break;
+            case 2:
+                // TODO: deposit
+                break;
+            case 3:
+                // TODO: view transactions
+                break;
+        }
+    } while (choice !== 4);
+
+    console.log("Terima kasih telah menggunakan layanan ATM. Sampai jumpa!");
     console.log("====================================");
-    console.log("Menu ATM:");
-    console.log("1. Cek Saldo");
-    console.log("2. Setor Tunai");
-    console.log("3. Riwayat Transaksi");
-    console.log("4. Keluar");
-
-    choice = await askQuestion("Masukkan pilihan Anda: ");
-
-    switch (parseInt(choice)) {
-      case 1:
-        // TODO: check balance
-        break;
-      case 2:
-        // TODO: deposit
-        break;
-      case 3:
-        // TODO: view transactions
-        break;
-    }
-  } while (choice !== 4);
-
-  console.log("Terima kasih telah menggunakan layanan ATM. Sampai jumpa!");
-  console.log("====================================");
-  rl.close();
+    rl.close();
 }
 
 main();
